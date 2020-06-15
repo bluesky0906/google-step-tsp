766714591b6a9579fdf141dbd796c5756f147484
Sora Tagami <loslonbervene.1923@icloud.com>
2020-06-15 15:09:35 +0900

    add NN

---
+++ solver_NN.py
@@ -0,0 +1,32 @@
+# NearestNeighbor法
+
+import sys
+
+import solver_greedy
+from common import print_tour, read_input
+
+
+def solve(cities):
+    N = len(cities)
+    # まだ探していない都市
+    unvisited_cities = set(range(N))
+    start_city = 0
+    tour = [start_city]
+    unvisited_cities.remove(start_city)
+
+    while unvisited_cities:
+        dist = [float('inf')] * N
+        for i, city in enumerate(unvisited_cities):
+            current_city = tour[-1]
+            dist[city] = solver_greedy.distance(
+                cities[city], cities[current_city])
+        next_city = dist.index(min(dist))
+        tour.append(next_city)
+        unvisited_cities.remove(next_city)
+    return tour
+
+
+if __name__ == '__main__':
+    assert len(sys.argv) > 1
+    tour = solve(read_input(sys.argv[1]))
+    print_tour(tour)
