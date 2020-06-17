266c8b9348e8930e4d935280a8b32f7f699dbba7
Sora Tagami <loslonbervene.1923@icloud.com>
2020-06-17 20:41:53 +0900

    update output generator

--- my_output_generator.py
+++ my_output_generator.py
@@ -3,6 +3,8 @@ from common import format_tour, read_input
 import solver_NN
 import solver_DNN
 import solver_2opt
+import solver_1_5opt
+import solver_comb
 
 CHALLENGES = 7
 
@@ -14,17 +16,26 @@ def choose_mode():
     mode = input()
     if mode == 'NN':
         solver = solver_NN
-        name = mode
     elif mode == 'DNN':
         solver = solver_DNN
-        name = mode
     elif mode == 'NN_2opt':
         solver = solver_2opt
         origin_solver = solver_NN
-        name = mode
+    elif mode == 'DNN_2opt':
+        solver = solver_2opt
+        origin_solver = solver_DNN
+    elif mode == 'NN_1_5opt':
+        solver = solver_1_5opt
+        origin_solver = solver_NN
+    elif mode == 'NN_comb':
+        solver = solver_comb
+        origin_solver = solver_NN
+    elif mode == 'DNN_comb':
+        solver = solver_comb
+        origin_solver = solver_NN
     else:
         exit(1)
-    return (solver, name, origin_solver)
+    return (solver, mode, origin_solver)
 
 
 def generate_sample_output(solver, name, origin_solver):
@@ -33,7 +44,8 @@ def generate_sample_output(solver, name, origin_solver):
         cities = read_input(f'input_{i}.csv')
         # 改善法も実行
         if origin_solver:
-            tour = solver.solve(cities, origin_solver)
+            tour = origin_solver.solve(cities)
+            tour = solver.solve(cities, tour)
         else:
             tour = solver.solve(cities)
         with open(f'my_output/{name}_{i}.csv', 'w') as f:
