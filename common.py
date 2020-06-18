03edd95341752f991f9d752a1ef8ea9f979c9925
Sora Tagami <loslonbervene.1923@icloud.com>
2020-06-18 15:39:41 +0900

    update common

--- common.py
+++ common.py
@@ -1,3 +1,6 @@
+import math
+
+
 def read_input(filename):
     with open(filename) as f:
         cities = []
@@ -13,3 +16,26 @@ def format_tour(tour):
 
 def print_tour(tour):
     print(format_tour(tour))
+
+
+def distance(city1, city2):
+    return math.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
+
+
+def get_all_distance(cities):
+    N = len(cities)
+    dist = [[float('inf')] * N for i in range(N)]
+    for i in range(N):
+        for j in range(i, N):
+            dist[i][j] = dist[j][i] = distance(
+                cities[i], cities[j])
+    return dist
+
+
+# 経路長
+def get_tour_length(tour, dist):
+    N = len(tour)
+    length = 0
+    for i in range(N):
+        length += dist[tour[i]][tour[(i+1) % N]]
+    return length
