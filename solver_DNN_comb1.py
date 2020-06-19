b45b40ac219bc586ae58419d1c6eaa2e2e98ec49
Sora Tagami <loslonbervene.1923@icloud.com>
2020-06-19 14:04:56 +0900

    update DNN comb1

--- solver_DNN_comb1.py
+++ solver_DNN_comb1.py
@@ -3,14 +3,14 @@ import sys
 # 改善アルゴリズム 2-opt + oropt法
 
 from common import print_tour, read_input, get_all_distance
-import solver_NN_2opt
+import solver_DNN_2opt
 import solver_NN_oropt
 
 
 def solve(cities):
     # 全ての距離
     dist = get_all_distance(cities)
-    tour = solver_NN_2opt.solve(cities)
+    tour = solver_DNN_2opt.solve(cities)
     solver_NN_oropt.improve_tour(dist, tour)
     return tour
 
